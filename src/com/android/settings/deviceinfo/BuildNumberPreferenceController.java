83eff432f947769f641e44e3dac09f8e778520e9
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-01-03 03:38:03 +0000

    Settings: Append Legacy version on build display id
    Change-Id: I831800ae0a769513b6b0c008ba492cd8334b3b65
    Signed-off-by: Karan Parashar <whyredfire@gmail.com>
    Signed-off-by: Kizziama <kizziama@proton.me>

--- src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -79,7 +79,14 @@ public class BuildNumberPreferenceController extends BasePreferenceController im
 
     @Override
     public CharSequence getSummary() {
-        return BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY);
+        StringBuilder sb = new StringBuilder();
+        sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
+        String legacyVersion = VersionUtils.getlegacyVersion();
+        if (!legacyVersion.equals("")){
+            sb.append("\n");
+            sb.append(legacyVersion);
+        }
+        return sb.toString();
     }
 
     @Override
---
+++ src/com/android/settings/deviceinfo/VersionUtils.java
@@ -0,0 +1,10 @@
+
+package com.android.settings.deviceinfo;
+
+import android.os.SystemProperties;
+
+public class VersionUtils {
+    public static String getlegacyVersion(){
+        return SystemProperties.get("org.legacy.version","");
+    }
+}
